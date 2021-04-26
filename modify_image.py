c2531458cfafc76bd9b036e062062b20028452c3
vorsanigo <verorsanigo@gmail.com>
2021-04-26 16:39:41 +0200

    Update modify_image.py

--- modify_image.py
+++ modify_image.py
@@ -77,8 +77,8 @@ def modifyImage(data_path):
             os.chdir(image_path)
 
             # Saving new images
-            cropl.save("cropl_%s.jpg" % (j))
-            '''cropup.save("cropup_%s.jpg" % (j))
+            '''cropl.save("cropl_%s.jpg" % (j))
+            cropup.save("cropup_%s.jpg" % (j))
             cropr.save("cropr_%s.jpg" % (j))
             cropdown.save("cropdown_%s.jpg" % (j))
             cropall.save("cropall_%s.jpg" % (j))
