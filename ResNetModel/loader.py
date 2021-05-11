eceb9666cf4c9f0cf53884d20c9aad1c51b2667d
vorsanigo <verorsanigo@gmail.com>
2021-05-11 14:23:00 +0200

    Update loader.py

--- ResNetModel/loader.py
+++ ResNetModel/loader.py
@@ -83,14 +83,15 @@ class Loader():
                 print(img)
                 print(img.shape) # (224, 224, 3)
 
-                img1 = np.random.rand(224, 224, 1)
+                img1 = np.random.rand(224, 224, 4)
                 # reshape dimension of channels to 3
                 if img1.shape[2] == 1:
-                    print("HEYYYYYYYYHEYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYHEYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY")
                     img1 = np.repeat(img1, 3, axis=2)
-                    print(img1.shape)
                 if img.shape[2] == 4:
+                    print("HEYYYYYYYYHEYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYHEYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY")
                     img = img[:, :, :2]
+                    print(img1.shape)
+
 
                 # preprocess the image by (1) expanding the dimensions and
                 # (2) subtracting the mean RGB pixel intensity from the
