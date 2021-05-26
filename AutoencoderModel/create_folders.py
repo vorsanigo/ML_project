f49250c5828a49755fc8db4031ff04a5000a59d2
AlbertoDeBenedittis <alberto.debenedittis@studenti.unitn.it>
2021-05-26 12:15:27 +0200

    Create classes

--- AutoencoderModel/create_folders.py
+++ AutoencoderModel/create_folders.py
@@ -1,6 +1,40 @@
+#from AutoencoderModel.main_triplets import OutputDir
 import shutil, os
+import glob 
+
+dir_path = 'C:\\Users\\alber\\Desktop\\UniTn\\Data Science\\Second_Semester\\Machine_Learning\\Nuova cartella\\ukbench'
 
 
-files = ['file1.txt', 'file2.txt', 'file3.txt']
-for i in range(files):
-    shutil.move(files[i], 'dest_folder')
+def read_imgs_no_subfolders(dirPath, extensions=None):
+    all_img = []
+    img_list = glob.glob(os.path.join(dirPath, '*'))
+
+    
+    """for img_path in img_list:
+        if img_path.endswith(ext):
+            head_tail = os.path.split(img_path)
+            image_name = head_tail[1]
+            all_img.append(image_name)"""
+    
+
+    return img_list
+
+
+
+files = read_imgs_no_subfolders(dir_path)
+#for i in range(files):
+i = 0 
+k = 1
+while i < len(files):
+    
+    for j in range(i,i+4):
+        OutputDir = 'C:\\Users\\alber\\Desktop\\UniTn\\Data Science\\Second_Semester\\Machine_Learning\\Nuova cartella\\Nuova_magica_cartella\\class_{}'.format(k)
+        
+        if not os.path.exists(OutputDir):
+            os.makedirs(OutputDir)
+        
+        shutil.copy(files[j], OutputDir)
+    
+    k += 1 
+    i += 4 
+
