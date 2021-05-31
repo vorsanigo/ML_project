5680e8043fa8ce14728a3927ebac49800b65e67d
AlbertoDeBenedittis <alberto.debenedittis@studenti.unitn.it>
2021-05-31 17:19:04 +0200

    Delete unused files

--- AutoencoderModel/dataset_augmentation.py
+++
@@ -1,5 +0,0 @@
-from simple_image_download import simple_image_download as simp
-
-response = simp.simple_image_download
-response().download('sagrada familia', 5)
-print(response().urls('sagrada familia', 5))
